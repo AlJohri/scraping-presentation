3fe8c7813cc7eddb9c2a838d78b2decd7e91af29
Mehta, Dhrumil <dhrumil.mehta@gmail.com>
2014-07-11 12:17:01 -0400

    clean songmeanings scraper, upload data

--- scrapers/songmeanings.py
+++ scrapers/songmeanings.py
@@ -1,13 +1,14 @@
-import re, csv, string
+import re
+import unicodecsv as csv
 import requests, lxml.html
 
 # letters = ['a', 'b'. 'c', ... 'x', 'y', 'z', 'numbers']
-letters = list(string.ascii_lowercase) + ['numbers']
+letters = ['c'] # only download letter c for demo
 
 pagination_regex = re.compile(r'Page \d+ of (\d+)')
 artist_id_regex = re.compile(r'http://songmeanings.com/artist/view/songs/(\d+)')
 
-letters = ['c'] # only download letter c for example script
+songs = []
 
 for letter in letters:
 
@@ -18,12 +19,14 @@ for letter in letters:
 	pagination_text = doc.cssselect('.song-info li:nth-child(2)')[0].text_content().strip()
 	pages = pagination_regex.findall(pagination_text)[0]
 
-	# redownloading page 1 for simplicity of example
-	# start from page 1 to number of pages
+	pages = 2 # only download 1 page for demo
+
+	# scrape pages
 	for page in range(1, int(pages)):
 		response = requests.get('http://songmeanings.com/artist/directory/%s/?page=%d' % (letter, page))
-		doc = lxml.html.fromstring(response.content)
-		artists = doc.cssselect("#artistslist table tbody tr")
+		artists_doc = lxml.html.fromstring(response.content)
+		artists = artists_doc.cssselect("#artistslist table tbody tr")
+
 		for artist in artists:
 			artist_url = artist.cssselect('a')[0].get('href')
 			artist_title = artist.cssselect('a')[0].get('title')
@@ -31,14 +34,20 @@ for letter in letters:
 			print artist_id + '\t' + artist_title
 
 			response = requests.get(artist_url)
-			doc = lxml.html.fromstring(response.content)
-			songs = doc.cssselect('#songslist tr')
-			for song in songs:
+			songs_doc = lxml.html.fromstring(response.content)
+			artist_songs = songs_doc.cssselect('#songslist tr')
+			for song in artist_songs:
 				song_id = song.get('id').replace("lyric-", "")
 				song_url = song.cssselect('a')[0].get('href')
 				song_title = song.cssselect('a')[0].text_content()
 				print "\t" + song_id.rjust(20) + '\t' + song_title
 
+				songs.append( (song_id, song_url, song_title, artist_id, artist_url, artist_title) )
+
 			print "-------------------------------------------------------"
 
-		# raw_input("Press Enter to continue...")
+# write to csv
+with open("songmeanings.csv", "w") as outfile:
+	writer = csv.writer(outfile)
+	writer.writerow(["song_id", "song_url", "song_title", "artist_id", "artist_url", "artist_title"])
+	writer.writerows(songs)
